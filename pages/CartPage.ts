5ede3f1d6279a725d4344239c405153d0e0b351c
ventsislavVidulov <xcen3k@gmail.com>
2025-12-22 15:32:29 +0200

    add method for scrapeing cart data

--- pages/CartPage.ts
+++ pages/CartPage.ts
@@ -1,6 +1,9 @@
 import { BasePage } from './BasePage';
 import { PaymentDetails } from '../types/PaymentDetails';
 
+import { CartItem } from '../types/CartItem';
+
+
 export class CartPage extends BasePage {
   // ===========================================================================
   // CART ACTIONS & MESSAGES
@@ -15,11 +18,17 @@ export class CartPage extends BasePage {
   // CART TABLE DATA
   // ===========================================================================
   readonly cartItemRows = () => this.page.locator('#cart_info_table tbody tr');
+  readonly cartItemNames = () => this.page.locator('.cart_description h4').allInnerTexts();
+  readonly cartItemPrices = () => this.page.locator('.cart_price p').allInnerTexts();
+  readonly cartItemPrice = (itemIndex = 1) => this.page.locator('.cart_price p').nth(itemIndex);
+  readonly cartItemQuantities = () => this.page.locator('.cart_quantity button.disabled').allInnerTexts();
+  readonly cartItemTotals = () => this.page.locator('.cart_total p').allInnerTexts();
+
   readonly btnQuantity = (itemIndex = 0) => this.page.locator('button.disabled').nth(itemIndex);
   readonly inputQuantity = (itemIndex = 0) => this.page.locator('input.cart_quantity_input').nth(itemIndex);
   readonly cartTotalPrice = () => this.page.locator('#cart_info_table .cart_total_price');
-  readonly cartItemPrices = () => this.page.locator('#cart_info_table .cart_price');
-  readonly cartItemTotalPrices = () => this.page.locator('#cart_info td.cart_total');
+  // readonly cartItemPrices = () => this.page.locator('#cart_info_table .cart_price');
+  // readonly cartItemTotalPrices = () => this.page.locator('#cart_info td.cart_total');
 
   // ===========================================================================
   // SPECIFIC CART ITEMS
@@ -33,4 +42,33 @@ export class CartPage extends BasePage {
   readonly modalCheckout = () => this.page.locator('#checkoutModal');
   readonly linkRegisterLoginCheckout = () => this.page.locator('#checkoutModal u');
   readonly msgRegisterLoginCheckout = () => this.page.locator('p:has-text("Register / Login account to proceed on checkout.")');
+
+  // ===========================================================================
+  // HELPER METHODS
+  // ===========================================================================
+  async scrapeCartItemsData() {
+    const cartItemsData: CartItem[] = [];
+    const cartItemNames = await this.cartItemNames();
+    const cartItemPrices = await this.cartItemPrices();
+    const cartItemQuantities = await this.cartItemQuantities();
+    const cartItemTotals = await this.cartItemTotals();
+
+    for (let i = 0; i < cartItemNames.length; i++) {
+      const cartItem: CartItem = {
+        name: cartItemNames[i],
+        price: parseFloat(cartItemPrices[i].replace(/[^0-9.]|(?<!\d)\./g, '')),
+        quantity: parseInt(cartItemQuantities[i]),
+        total: parseFloat(cartItemTotals[i].replace(/[^0-9.]|(?<!\d)\./g, ''))
+      };
+
+      cartItemsData.push(cartItem);
+    }
+
+    return cartItemsData;
+  }
+
+  async getTotalPrice(): Promise<number> {
+    const cartItemsData = await this.scrapeCartItemsData();
+    return cartItemsData.reduce((total, item) => total + item.total, 0);
+  }
 }
